eb47af36d48fa8adce8306944d62877d85dad971
newmaker3 <75644931+newmaker3@users.noreply.github.com>
2021-01-01 21:27:53 -0600

    Rewrite glitch.lua
    This massively improves performance.

--- lua/glitch.lua
+++ lua/glitch.lua
@@ -7,39 +7,20 @@ else
 	file = args[1]
 end
 
-f = io.open(file,"rb") --open gif
-dat = f:read("*a") --read all gif data
-f:close() --close gif
-io.stdout:write("Glitching "..file)
-tdat = {}
-local i = 0
-local mod = math.floor(#dat/4)
+f = io.open(file,"rb")
+dat = f:read("*a") 
+f:close() 
 
-for c in dat:gmatch(".") do --convert gif data(string) to a table
-	if i % mod == 0 then io.stdout:write(".") end
-	table.insert(tdat,c) 
-	i = i + 1
+local e = dat:find(string.char(33,255))-- find color table end
+if not e then
+	e = dat:find(string.char(33,249))
 end
 
-local e
-for i = 1,#tdat do --find end of color table
-	if string.byte(tdat[i]) == 33 then
-		if string.byte(tdat[i + 1]) == 255 then
-			e = i
-			break
-		elseif string.byte(tdat[i + 1]) == 249 then
-			e = i
-			break
-		end
-	end
-end
-math.randomseed(os.time())
-for i = 13,e-1 do--randomize color table
-	tdat[i] = string.char(math.random(0,255))
-end
 
-outfile = file:gsub("%.gif","-glitched.gif")
-f = io.open(outfile,"wb")
-f:write(table.concat(tdat))
-f:close()
-print("\ndone!")
+f = io.open("out.gif","wb")
+f:write(dat:sub(1,13))-- write gif header
+for i = 1,e - 14 do-- write randomized color table
+	f:write(string.char(math.random(0,255)))
+end
+f:write(dat:sub(e,-1))-- write the rest of the gif data
+f:close(
