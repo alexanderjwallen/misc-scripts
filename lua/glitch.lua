1fbb2d1e56cf5269df3f11d672ebf6575c5eb466
newmaker3 <75644931+newmaker3@users.noreply.github.com>
2021-01-07 00:15:41 -0600

    You guessed it, another rewrite!
    This time with proper header reading

--- lua/glitch.lua
+++ lua/glitch.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/lua
 args = {...}
+local sb = string.byte
 if not args[1] then
 	io.stdout:write("Please enter file name: ")
 	file = io.read()
@@ -7,20 +8,33 @@ else
 	file = args[1]
 end
 
-f = io.open(file,"rb")
-dat = f:read("*a") 
-f:close() 
+fin = io.open(file,"rb")
 
-local e = dat:find(string.char(33,255))-- find color table end
-if not e then
-	e = dat:find(string.char(33,249))
+fout = io.open(file:gsub(".gif","").."-glitched.gif","wb")
+
+for i = 1,13 do
+	fout:write(fin:read(1)) -- write header, this will always be the same
 end
 
+fin:seek("set",10) -- seek to the 9th byte of the file(yes, 9th because even files start at 1 in lua)
+gflags = sb(fin:read(1)) -- read global flags
 
-f = io.open("out.gif","wb")
-f:write(dat:sub(1,13))-- write gif header
-for i = 1,e - 14 do-- write randomized color table
-	f:write(string.char(math.random(0,255)))
+if bit32.rshift(gflags,7) == 1 then --if we have a global color table, write random garbage to it to "glitch" it
+	local a = bit32.band(gflags,7) + 1
+	local b = 2 ^ a
+	local gcolorlen = 3 * b
+	math.randomseed(os.time())
+	for i = 1,gcolorlen-1 do
+		fout:write(string.char(math.random(0,255)))
+	end
+else
+	error("no global table")
+	fin:close()
+	fout:close()
 end
-f:write(dat:sub(e,-1))-- write the rest of the gif data
-f:close(
+
+fin:seek("set",12+gcolorlen) -- seek to the byte right after the global color table
+fout:write(fin:read("*all"))
+
+fin:close()
+fout:close()
